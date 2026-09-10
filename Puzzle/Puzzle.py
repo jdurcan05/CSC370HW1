1bd1ffa67cb36f4f4f0ec2bb36b648efb2f6f73b
Tyler6969-sudo <77254070+tylerthompson17@users.noreply.github.com>
2026-09-10 14:24:27 -0400

    added solvability, have not tested yet

--- Puzzle/Puzzle.py
+++ Puzzle/Puzzle.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Puzzle:
     def generate(self):
         while 1:
@@ -10,4 +11,22 @@ class Puzzle:
 
     @staticmethod
     def _is_solvable(board):
-        return 1
+        serpent = board
+        serpent[3:5] = serpent[3:5][::-1]
+
+        cycles = 0
+
+        for i in range(8):
+            cycles += Puzzle._is_solvable_helper(board, serpent, i)
+
+        return not (8 - cycles % 2)
+
+    @staticmethod
+    def _is_solvable_helper(board, serpent, i):
+        if serpent[i] == board[i]:
+            return 1
+
+        val = serpent[i]
+        new_index = board.index(val)
+        Puzzle._is_solvable_helper(board, serpent, new_index)
+        return 0
